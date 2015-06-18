cc4ab8e29cbfefe37aaf62a95e5b04f23605fdb1
Celena Ng <celena.ng@goodshop.com>
2015-06-18 12:54:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4fac1a34579fad812c878f1bf383c9f58cfd7766d47d404a6342854abae706ce375ff1893a4cd00be09e25338876be2228b1b0adab20676014c2e9d055747d0c'
